46cfc7f1c7597c9efe2a5a5ec9320d60aeb3ff3d
its-mohammad-js <mohammadreactjs@gmail.com>
2024-02-02 09:18:51 +0330

    deploy test branch initial

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,6 +4,7 @@ import { routeInfo } from "../helpers/constants";
 function Footer() {
   return (
     <div className="bg-yellow-200 flex justify-evenly">
+      <h1>Footer</h1>
       {routeInfo.map(({ name, path }, index) => (
         <Link key={index} to={path}>
           {name}
