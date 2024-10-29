df0c58b163ce1f945b31737a71e9791909020d0f
its-mohammad-js <mohammadreactjs@gmail.com>
2024-10-29 13:47:34 +0330

    test: gh actions

--- src/common/utils/removeExpiredStories.mjs
+++ src/common/utils/removeExpiredStories.mjs
@@ -1,43 +1,5 @@
-import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  setDoc,
-  doc,
-  collection,
-  query,
-  where,
-  getDocs,
-} from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCdnuxel4imeAOVQVogRiHvqvrXb5qVRQw",
-  authDomain: "ecovibe-c6720.firebaseapp.com",
-  databaseURL: "https://ecovibe-c6720-default-rtdb.firebaseio.com",
-  projectId: "ecovibe-c6720",
-  storageBucket: "ecovibe-c6720.appspot.com",
-  messagingSenderId: "944059551615",
-  appId: "1:944059551615:web:2f96d7e9a67d0065e9544a",
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// check createAt date
-function isTwoDaysPassed(dateObject) {
-  if (!dateObject) return false;
-  // Convert the date object to a JavaScript Date object
-  const date = new Date(
-    dateObject.seconds * 1000 + dateObject.nanoseconds / 1000000
-  );
-  // Calculate the current time
-  const now = new Date();
-  // Calculate the difference in milliseconds
-  const difference = now.getTime() - date.getTime();
-  // Convert milliseconds to days
-  const daysPassed = difference / (1000 * 60 * 60 * 24);
-  // Check if two days have passed
-  return daysPassed >= 2;
-}
+import { collection, query, getDocs } from "firebase/firestore";
+import { db } from "/src/config/firebase";
 
 async function addDocumentToFirestore() {
   try {
@@ -48,20 +10,14 @@ async function addDocumentToFirestore() {
       }
     ).then((time) => time.json());
 
-    // fiveMinutesAgo.setMinutes(fiveMinutesAgo.getMinutes() - 5);
-
-    const ref = query(
-      collection(db, "Stories")
-      // where("createdAt", ">=", fiveMinutesAgo),
-      // where("createdAt", "<", new Date())
-    );
+    const ref = query(collection(db, "Stories"));
 
     const docs = await getDocs(ref).then(({ docs }) =>
       docs.map((doc) => ({ ...doc.data(), id: doc.id }))
     );
 
     docs.forEach((doc, i) => {
-      console.log(doc.createdAt, date.unixTime);
+      console.log(doc.createdAt, time.unixTime);
     });
   } catch (error) {
     console.error("Error on whole proccess");
