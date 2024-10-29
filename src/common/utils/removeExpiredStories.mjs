6be7fa2436faa11e0cd05d5fa290b763f542afdb
its-mohammad-js <mohammadreactjs@gmail.com>
2024-10-29 13:49:01 +0330

    test: gh actions

--- src/common/utils/removeExpiredStories.mjs
+++ src/common/utils/removeExpiredStories.mjs
@@ -1,5 +1,18 @@
-import { collection, query, getDocs } from "firebase/firestore";
-import { db } from "/src/config/firebase";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, query, getDocs } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdnuxel4imeAOVQVogRiHvqvrXb5qVRQw",
+  authDomain: "ecovibe-c6720.firebaseapp.com",
+  databaseURL: "https://ecovibe-c6720-default-rtdb.firebaseio.com",
+  projectId: "ecovibe-c6720",
+  storageBucket: "ecovibe-c6720.appspot.com",
+  messagingSenderId: "944059551615",
+  appId: "1:944059551615:web:2f96d7e9a67d0065e9544a",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 async function addDocumentToFirestore() {
   try {
