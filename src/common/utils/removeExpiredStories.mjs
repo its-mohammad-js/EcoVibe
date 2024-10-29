4fdee1f054d31b3e9a84d3a5ac0b81b202e4d39a
its-mohammad-js <mohammadreactjs@gmail.com>
2024-10-29 12:34:31 +0330

    test: gh actions

--- src/common/utils/removeExpiredStories.mjs
+++ src/common/utils/removeExpiredStories.mjs
@@ -41,41 +41,43 @@ function isTwoDaysPassed(dateObject) {
 
 async function addDocumentToFirestore() {
   try {
-    const { unixtime } = await fetch(
+    const date = await fetch(
       "http://worldtimeapi.org/api/timezone/America/New_York",
       {
         method: "GET",
       }
     ).then((time) => time.json());
+    console.log(date);
+
     // fiveMinutesAgo.setMinutes(fiveMinutesAgo.getMinutes() - 5);
 
-    const ref = query(
-      collection(db, "Stories")
-      // where("createdAt", ">=", fiveMinutesAgo),
-      // where("createdAt", "<", new Date())
-    );
+    // const ref = query(
+    //   collection(db, "Stories")
+    //   // where("createdAt", ">=", fiveMinutesAgo),
+    //   // where("createdAt", "<", new Date())
+    // );
 
-    const docs = await getDocs(ref).then(({ docs }) =>
-      docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-    );
+    // const docs = await getDocs(ref).then(({ docs }) =>
+    //   docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+    // );
 
-    docs.map(async (data, i) => {
-      try {
-        console.log(data.createdAt, unixtime);
+    // docs.map(async (data, i) => {
+    //   try {
+    //     console.log(data.createdAt, unixtime);
 
-        // const docRef = doc(collection(db, "newCollection"), data.id);
-        // await setDoc(docRef, {
-        //   ...data,
-        //   addedField: {
-        //     test: true,
-        //     message: "test is successfully done",
-        //   },
-        // });
-        // console.log(`${i + 1}st doc has changed`);
-      } catch (error) {
-        console.log("error on updating doc", error);
-      }
-    });
+    //     // const docRef = doc(collection(db, "newCollection"), data.id);
+    //     // await setDoc(docRef, {
+    //     //   ...data,
+    //     //   addedField: {
+    //     //     test: true,
+    //     //     message: "test is successfully done",
+    //     //   },
+    //     // });
+    //     // console.log(`${i + 1}st doc has changed`);
+    //   } catch (error) {
+    //     console.log("error on updating doc", error);
+    //   }
+    // });
 
     // console.log(`successefully finded ${docs?.length} stories`);
   } catch (error) {
