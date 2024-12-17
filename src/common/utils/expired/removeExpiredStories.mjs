bb3a62e8807bf9f08e494829aca916eabcaec603
its-mohammad-js <mohammadreactjs@gmail.com>
2024-12-17 06:34:42 +0330

    update remove expried slides function

--- src/common/utils/expired/removeExpiredStories.mjs
+++ src/common/utils/expired/removeExpiredStories.mjs
@@ -38,8 +38,7 @@ function checkIsExpired(timestamp) {
   const difference = utcNow.getTime() - utcDate.getTime();
 
   // Convert milliseconds to hours
-  // const hoursPassed = difference / (1000 * 60 * 60);
-  const hoursPassed = difference / (1000 * 60);
+  const hoursPassed = difference / (1000 * 60 * 60);
 
   // Check if at least 18 hours have passed (adjust the threshold as needed)
   return hoursPassed >= 2;
