5cb958c221e671db39c86bb02cec4e39de07a13e
its-mohammad-js <mohammadreactjs@gmail.com>
2024-12-17 12:38:27 +0330

    test & update remove canceled orders action

--- src/common/utils/expired/RemoveExpiredOrders.mjs
+++ src/common/utils/expired/RemoveExpiredOrders.mjs
@@ -50,42 +50,8 @@ async function removeCanceledOrders() {
     docs.forEach(async (order, i) => {
       try {
         if (checkIsExpired(order.createdAt)) {
-          // Step 1: Convert the `orders` property (object) into an array of entries
-          const ordersArray = Object.entries(order.orders); // [sellerId, orderData]
-
-          // Step 2: Filter the orders based on `delivery_status`
-          const filteredOrders = ordersArray.filter(([sellerId, orderData]) => {
-            return orderData.delivery_status !== 900;
-          });
-
-          // Step 3: Map the filtered orders back to an object
-          const updatedOrders = Object.fromEntries(filteredOrders);
-
-          // Step 4: Update the `sellers` array to exclude seller IDs of removed orders
-          const removedSellerIds = ordersArray
-            .filter(
-              ([sellerId, orderData]) => orderData.delivery_status === 900
-            ) // Get removed orders
-            .map(([sellerId]) => sellerId); // Extract seller IDs
-
-          const updatedSellers = order.sellers.filter(
-            (sellerId) => !removedSellerIds.includes(sellerId)
-          );
-
           const orderDocRef = doc(db, "Orders", order.id); // `order.id` is assumed to be the document ID
-
-          // remove order if it was the last ordered items
-          if (Object.values(updatedSellers).length <= 0) {
-            deleteDoc(orderDocRef);
-          } else {
-            // update order data with new values
-            await updateDoc(orderDocRef, {
-              orders: updatedOrders,
-              sellers: updatedSellers,
-            });
-          }
-
-          console.log(`Successfully updated Order ${i + 1}st in Firestore.`);
+          deleteDoc(orderDocRef);
         } else {
           console.log("order isn't expired yet");
         }
