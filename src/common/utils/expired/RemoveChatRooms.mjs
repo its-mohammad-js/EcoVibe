9c6cee874e2ab297ec0e9159e79ca430f93b589f
its-mohammad-js <mohammadreactjs@gmail.com>
2024-12-18 15:58:16 +0330

    add clean expired chat rooms action

---
+++ src/common/utils/expired/RemoveChatRooms.mjs
@@ -0,0 +1,47 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, onValue, ref } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+// check createAt date
+function checkIsExpired(dateObject) {
+  if (!dateObject) return false;
+  // Convert the date object to a JavaScript Date object
+  const date = new Date(
+    dateObject.seconds * 1000 + dateObject.nanoseconds / 1000000
+  );
+  // Calculate the current time
+  const now = new Date();
+  // Calculate the difference in milliseconds
+  const difference = now.getTime() - date.getTime();
+  // Convert milliseconds to days
+  const daysPassed = difference / (1000 * 60 * 60 * 24);
+  // Check if 7 days have passed
+  return daysPassed >= 7;
+}
+
+async function removeCanceledOrders() {
+  try {
+    const db = getDatabase();
+    const roomsRef = ref(db, "rooms");
+
+    onValue(roomsRef, (snapshot) => {
+      console.log(Object.values(snapshot.val()));
+    });
+  } catch (error) {
+    console.error("Error on whole proccess");
+    throw error; // Re-throw error for GitHub Action to fail
+  }
+}
+
+removeCanceledOrders();
