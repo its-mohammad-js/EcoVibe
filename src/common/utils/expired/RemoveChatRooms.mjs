ee1b26b21cd2249806df09653142454f6ace0f64
its-mohammad-js <mohammadreactjs@gmail.com>
2024-12-19 07:08:21 +0330

    update& test clean expired chat rooms action

--- src/common/utils/expired/RemoveChatRooms.mjs
+++ src/common/utils/expired/RemoveChatRooms.mjs
@@ -33,8 +33,10 @@ async function checkIsExpired([firstDate, secondDate]) {
   const serverTime = Date.now() + serverTimeOffset;
 
   // Helper to check expiration
+  // const isExpired = (date) =>
+  //   Math.floor((serverTime - date) / (24 * 60 * 60 * 1000)) >= 7;
   const isExpired = (date) =>
-    Math.floor((serverTime - date) / (24 * 60 * 60 * 1000)) >= 7;
+    Math.floor((serverTime - date) / (60 * 1000)) >= 7;
 
   // Return the result
   return isExpired(firstDate) && isExpired(secondDate);
