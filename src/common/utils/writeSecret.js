bccd59fcd7901bea4f43ae6b5e61a6f28814e9db
its-mohammad-js <mohammadreactjs@gmail.com>
2024-12-20 14:01:07 +0330

    test and update remove inactive users

---
+++ src/common/utils/writeSecret.js
@@ -0,0 +1,32 @@
+const fs = require("fs-extra");
+
+async function createServiceAccountFile() {
+  const secret = process.env.FIREBASE_SECRET;
+
+  if (!secret) {
+    console.error("FIREBASE_SECRET is not set.");
+    process.exit(1);
+  }
+
+  try {
+    // Parse the secret to ensure it's valid JSON
+    const parsedSecret = JSON.parse(secret);
+
+    // File path where the service account key will be stored
+    const filePath =
+      "./src/common/utils/expired/firebaseServiceAccountKey.json";
+
+    // Write the parsed JSON to the file
+    await fs.outputJson(filePath, parsedSecret, { spaces: 2 });
+
+    console.log(`Service account key successfully written to ${filePath}`);
+  } catch (err) {
+    console.error(
+      "Error parsing or writing Firebase service account key:",
+      err.message
+    );
+    process.exit(1);
+  }
+}
+
+createServiceAccountFile();
