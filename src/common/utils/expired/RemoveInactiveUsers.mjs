8be4702452fd3eb9b5a7a7caf1be02017d4461ab
its-mohammad-js <mohammadreactjs@gmail.com>
2024-12-20 05:29:08 +0330

    test and update remove inactive users

--- src/common/utils/expired/RemoveInactiveUsers.mjs
+++ src/common/utils/expired/RemoveInactiveUsers.mjs
@@ -23,28 +23,36 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// // check createAt date
-// function checkIsExpired(dateObject) {
-//   if (!dateObject) return false;
-//   // Convert the date object to a JavaScript Date object
-//   const date = new Date(
-//     dateObject.seconds * 1000 + dateObject.nanoseconds / 1000000
-//   );
-//   // Calculate the current time
-//   const now = new Date();
-//   // Calculate the difference in milliseconds
-//   const difference = now.getTime() - date.getTime();
-//   // Convert milliseconds to hours
-//   const hoursPassed = difference / (1000 * 60 * 60);
-//   // Check if 12 hours have passed
-//   return hoursPassed >= 12;
-// }
+// check createAt date
+function checkIsExpired(dateObject) {
+  console.log(dateObject ? "it has activity" : "no date object");
+
+  //   if (!dateObject) return false;
+  //   // Convert the date object to a JavaScript Date object
+  //   const date = new Date(
+  //     dateObject.seconds * 1000 + dateObject.nanoseconds / 1000000
+  //   );
+  //   // Calculate the current time
+  //   const now = new Date();
+  //   // Calculate the difference in milliseconds
+  //   const difference = now.getTime() - date.getTime();
+  //   // Convert milliseconds to hours
+  //   const hoursPassed = difference / (1000 * 60 * 60);
+  //   // Check if 12 hours have passed
+  //   return hoursPassed >= 12;
+}
 
 async function removeExpiredProducts() {
   try {
     const usersRef = collection(db, "users");
 
-    await getDocs(usersRef).then(({ docs }) => console.log(docs));
+    const allUsersData = await getDocs(usersRef).then(({ docs }) =>
+      docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+    );
+
+    allUsersData.forEach((user, i) => {
+      checkIsExpired(user.lastActivity);
+    });
   } catch (error) {
     console.error("Error during the user cleanup process:", error);
     throw error; // Re-throw for error monitoring
