e2c703b8ff40cb1b171a0f5a9795e2528bc76275
its-mohammad-js <mohammadreactjs@gmail.com>
2024-12-20 10:02:00 +0330

    test and update remove inactive users

--- src/common/utils/expired/RemoveInactiveUsers.mjs
+++ src/common/utils/expired/RemoveInactiveUsers.mjs
@@ -1,26 +1,16 @@
 import { readFile } from "fs/promises";
 import path from "path";
-import admin from "firebase-admin";
 
 const serviceAccountPath = path.resolve(
   process.cwd(),
-  "src/common/utils/expired/firebaseServiceAccountKey.json" // Updated to match the GitHub Action path
+  "src/common/utils/expired/firebaseServiceAccountKey.json"
 );
 
 try {
   const fileContents = await readFile(serviceAccountPath, "utf8");
-
-  console.log(fileContents.split("***")[1]);
-
-  //   console.log("Raw File Contents:", fileContents); // Add this line to debug
-  //   const serviceAccount = JSON.parse(fileContents); // Parse the JSON
-  //   console.log("Parsed Service Account:", serviceAccount);
-
-  if (!admin.apps.length) {
-    admin.initializeApp({
-      credential: admin.credential.cert(fileContents.split("***")[1]),
-    });
-  }
+  console.log("Raw File Contents:", fileContents); // Debugging line
+  const serviceAccount = JSON.parse(fileContents); // Parse JSON
+  console.log("Parsed Service Account:", serviceAccount); // Debugging line
 } catch (error) {
   console.error("Error reading or parsing JSON:", error.message, error.stack);
 }
