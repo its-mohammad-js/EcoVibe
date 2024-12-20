262f23b7eac54b5df5861342a166046a9f816c31
its-mohammad-js <mohammadreactjs@gmail.com>
2024-12-20 12:58:22 +0330

    test and update remove inactive users

--- src/common/utils/expired/RemoveInactiveUsers.mjs
+++ src/common/utils/expired/RemoveInactiveUsers.mjs
@@ -10,7 +10,7 @@ try {
   // Read and log the service account JSON contents
   const fileContents = await readFile(serviceAccountPath, "utf8");
 
-  console.log(fileContents.split("***")[1]);
+  console.log(fileContents.split("***"));
 
   // Parse the JSON
   const serviceAccount = JSON.parse(`{${fileContents}}`);
