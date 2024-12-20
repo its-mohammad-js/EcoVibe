f1dacfc6090da9c14c91e5c0676a797e5666f84f
its-mohammad-js <mohammadreactjs@gmail.com>
2024-12-20 13:28:22 +0330

    test and update remove inactive users

--- src/common/utils/expired/RemoveInactiveUsers.mjs
+++ src/common/utils/expired/RemoveInactiveUsers.mjs
@@ -11,13 +11,14 @@ async function initializeFirebase() {
   try {
     // Use fs-extra to read and parse the JSON
     const serviceAccount = await fs.readJson(serviceAccountPath);
+    console.log(serviceAccount);
 
     // Initialize Firebase Admin SDK
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    });
+    // admin.initializeApp({
+    //   credential: admin.credential.cert(serviceAccount),
+    // });
 
-    console.log("Firebase Admin SDK initialized successfully!");
+    // console.log("Firebase Admin SDK initialized successfully!");
   } catch (error) {
     console.error("Error initializing Firebase Admin SDK:", error);
   }
